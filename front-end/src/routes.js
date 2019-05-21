ba92f691beeae99494f4f5ccc72ae3ecd3cfe60e
Muhammad Husnain <husnain.shahzad@capazyte.com>
2019-05-22 01:45:05 +0500

    hcurnor updates

--- front-end/src/routes.js
+++ front-end/src/routes.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
-import {Route, Router, browserHistory} from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import Home from './containers/Home/Home';
+import Shipments from './containers/Shipments/Shipments';
 
 
 
 const Routes = () => {
     return (
         <Router history={browserHistory}>
-            <Route path="/" component={Home}/>
-            <Route path="/Home" component={Home}/>
+            <Route path="/" component={Home}>
+                <IndexRoute component={Shipments} />
+                <Route path="" component={Shipments}/>
+            </Route>
         </Router>
     )
 };
