2c5dd8c9d49f8e206d0b37c80a361e19bcdce3b6
Syed Afzal <sah.afzal@gmail.com>
2019-05-22 02:23:18 +0500

    orderby on different fields

--- front-end/src/containers/Shipments/Item/Item.js
+++ front-end/src/containers/Shipments/Item/Item.js
@@ -15,9 +15,9 @@ export default class Item extends React.Component {
         return (
             <Card className="shipment-card">
                 <CardBody>
-                    <CardTitle>Card title</CardTitle>
-                    <CardSubtitle>Card subtitle</CardSubtitle>
-                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                    <CardTitle>{id}</CardTitle>
+                    <CardSubtitle>{name}</CardSubtitle>
+                    <CardText>{cell}</CardText>
                     <Button>Button</Button>
                 </CardBody>
             </Card>
--- front-end/src/containers/Shipments/Shipments.js
+++ front-end/src/containers/Shipments/Shipments.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Item from './Item/Item'
 import {Container, Row, Col} from "reactstrap";
+import { FormGroup, Label, Input } from 'reactstrap';
 import './Shipments.scss'
 
 export default class Shipments extends React.Component {
@@ -10,33 +11,33 @@ export default class Shipments extends React.Component {
         let data = [
             {
                 id: 1,
-                name: 'a',
-                cell: '019'
+                name: 'ballon',
+                cell: '9'
             },
             {
                 id: 2,
-                name: 'a',
-                cell: '019'
+                name: 'afzal',
+                cell: '7'
             },
             {
                 id: 3,
-                name: 'a',
-                cell: '019'
+                name: 'muneeb',
+                cell: '6'
             },
             {
                 id: 5,
-                name: 'a',
-                cell: '019'
+                name: 'zubair',
+                cell: '8'
             },
             {
                 id: 9,
-                name: 'a',
-                cell: '019'
+                name: 'ali',
+                cell: '5'
             },
             {
                 id: 8,
-                name: 'a',
-                cell: '019'
+                name: 'husnain',
+                cell: '1'
             }
         ];
         this.state = {
@@ -44,9 +45,46 @@ export default class Shipments extends React.Component {
         }
     }
 
+    _orderShipments = (e) => {
+        let newdata = this.state.data.sort(this._sort(e.target.value));
+        this.setState({
+            data: newdata
+        })
+    };
+
+    _sort = (property) => {
+        let sortOrder = 1;
+        if(property[0] === "-") {
+            sortOrder = -1;
+            property = property.substr(1);
+        }
+        return function (a,b) {
+            let result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+            return result * sortOrder;
+        }
+    };
+
+
     render() {
         return (
             <Container className="shipments">
+                <div className="filter-shipments">
+                    <Row>
+                        <Col  xl="3" lg="4" md="6" sm="6" xs="12">
+                            <FormGroup>
+                                <Label for="exampleSelect">Order Shipments By : </Label>
+                                <Input type="select" name="select" id="exampleSelect" onChange={this._orderShipments}>
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Input>
+                            </FormGroup>
+                        </Col>
+                    </Row>
+                </div>
+
                 <Row>
                     {
                         this.state.data.length > 0 ?
